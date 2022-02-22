04732fdbb49f1d8d9ed00e68a2c16f23567e4026
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-02-22 08:04:09 +0100

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] tablica = { "abba", "deska" };
+
+foreach (var item in tablica)
+{
+    Console.WriteLine(Check(item));
+}
+
+
+static bool Check(string text)
+{
+    if (string.IsNullOrWhiteSpace(text))
+    {
+        return false;
+    }
+
+    text = text.Replace(" ", "").ToLower();
+
+    return text == new string(text.ToCharArray().Reverse().ToArray());
+}
